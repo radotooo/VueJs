f9e8ff16aaf1557e83869350373ef397dadcf899
Radoto <radotooo@gmail.com>
2020-05-06 06:48:10 +0300

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div id="demo">
+  <button v-on:click="isHidden = !isHidden">
+    Toggle
+  </button>
+  <br>
+  <transition name="bounce">
+   <img :src="imgUrl" v-if="isHidden">  
+  </transition>
+  <br>
+<div class="container"  v-for="(f,index) in fruits" v-bind:key="index">
+  
+<div class="containerIn" v-bind:class="changeFontColor(index)">
+<h1 > v-bind:cl{{f.name}}</h1>
+<h2>{{f.age}}</h2>
+<h2>{{index}}</h2>
+</div>
+ 
+
+</div>
+   <!-- <h2 @mouseover="isHidden=!isHidden" >Time is ....</h2>
+  <div v-if="isHidden"> {{currnetDate}} </div> -->
+  </div> 
+
+  
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
-}
+  },
+  data: ()=>({
+   imgUrl:"https://www.ing.be/assets/nuid/images/uploads/How-to-read-a-URL-video1-en.png",
+    isHidden:true,
+    currnetDate: new Date(),
+    fruits : [
+      {id:1,name: "kiwi", age:50},
+      {id:2,name: "kiwi2", age:52},
+      {id:3,name: "kiwi3", age:53},
+      {id:4,name: "kiwi4", age:53},
+      {id:5,name: "kiwi5", age:53},
+
+      ]
+    }),
+    methods:{
+       changeFontColor(index) {
+        
+        return index%2 == 0 ? 'odd' : 'even'
+
+      }
+    }
+
+  };
+  
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.containerIn{
+  display: inline-block;
+        margin: auto;
+        padding: 3px;
+        background-color: #8ebf42
+}
+.container {
+  display: inline-block;
+      margin:10px
+         }
+.odd{
+  color:red;
+  border: 5px solid black;
+background-color: cornflowerblue;
+}
+.even{
+  color: lightblue;
+  background-color: pink;
+  border: 5px solid black;
+  
+}
+.bounce-enter-active {
+  
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 h3 {
   margin: 40px 0 0;
 }
