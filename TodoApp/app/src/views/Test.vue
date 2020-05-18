0f963df4b6cebfd555d24f9b594f0efa2bc5bc24
Radoto <radotooo@gmail.com>
2020-05-18 22:00:44 +0300

    Added input field

--- TodoApp/app/src/views/Test.vue
+++ TodoApp/app/src/views/Test.vue
@@ -8,48 +8,55 @@
       >
         <v-card-title>Top 10 Australian beaches</v-card-title>
       </v-img>
+
       <v-card-text class="title">
         <div v-for="(test,i) in testData" :key="i">
-        <v-row align="center" >
-          <v-checkbox 
-          name="rado" 
-          :label="test" 
-          color="success"></v-checkbox>
-          
-           </v-row>
+          <v-row align="center">
+            <v-checkbox name="rado" :label="test" color="success"></v-checkbox>
+            <v-icon color="red">{{trash}}</v-icon>
+          </v-row>
           <v-divider></v-divider>
-          </div>
-     
-      
+        </div>
       </v-card-text>
-     
-      <v-card-actions>
- <v-form  >
-        
-        <v-text-field
-                           
-                             placeholder="Placeholder"
-                           
-        ></v-text-field>
-        
-      </v-form>
-      </v-card-actions>
+
+      <v-card-actions></v-card-actions>
     </v-card>
+    <v-row class="pt-3" justify="center">
+      <v-form ref="form" align="center">
+        <v-text-field
+          required
+          v-on:keyup.enter="pushTask()"
+          style="width:400px"
+          v-model="task"
+          outlined
+          label="Add task"
+        ></v-text-field>
+      </v-form>
+    </v-row>
   </v-app>
 </template>
   
 <script>
-import { mdiPlaylistPlus } from '@mdi/js';
+import { mdiDeleteCircle } from "@mdi/js";
 
 export default {
   name: "Test",
   data: () => ({
-    testData: ["rado", "rado2", "rado3"]
+    testData: ["rado", "rado2", "rado3"],
+    task: "",
+    trash: mdiDeleteCircle
   }),
-  icon:mdiPlaylistPlus
+
+  methods: {
+    pushTask: function() {
+      if (!this.task == "") {
+        this.testData.push(this.task);
+        this.task = "";
+      }
+    }
+  }
 };
 </script>
 
 <style>
-
 </style>
